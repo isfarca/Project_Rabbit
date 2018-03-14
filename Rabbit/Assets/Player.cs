dc340ad02c1df28e1cf47e6f4952fa7d18abd580
Fethi Isfarca <fisfarca@googlemail.com>
2018-03-14 22:02:27 +0100

    Provisional grass texture
    Add the terrain with provisional grass texture and player.

---
+++ Rabbit/Assets/Player.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Value types
+    private float x, z;
+    private float speed = 3f;
+
+    // Use this for initialization
+    void Start()
+    {
+	}
+	
+	// Update is called once per frame
+	void Update()
+    {
+        // Player movement.
+        x = Input.GetAxis("Horizontal") * Time.deltaTime * speed; // Move right / left.
+        z = Input.GetAxis("Vertical") * Time.deltaTime * speed; // Move forward / backward
+
+        transform.Translate(x, 0, z);
+    }
+}
